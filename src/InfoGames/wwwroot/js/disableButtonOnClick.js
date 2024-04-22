b75fbdba94dce02c018c2a063c08cada2dd29bd6
RafaelMourao <mourao.ea@gmail.com>
2024-04-22 09:32:12 -0300

    feat: Desabilita o botao 'sincronizar' ao clicar

---
+++ src/InfoGames/wwwroot/js/disableButtonOnClick.js
@@ -0,0 +1,8 @@
+﻿// Disables buttons with class 'disable-button-on-click' when clicked
+// Requires jQuery
+$(document).ready(function () {
+    $('.disable-button-on-click').click(function () {
+        $(this).prop('disabled', true);
+        $(this).addClass('disabled');
+    });
+});
