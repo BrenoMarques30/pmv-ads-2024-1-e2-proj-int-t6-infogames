e90c78b0067bcbad97a692ff588aa3640e3d77f7
RafaelMourao <mourao.ea@gmail.com>
2024-04-23 19:14:36 -0300

    nao sei

---
+++ src/InfoGames/Models/Jogo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InfoGames.Models {
+    [Table("Jogos")]
+    public class Jogo {
+        [Key]
+        public required string Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o AppId (Id do jogo na Loja)")]
+        public required string AppId { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome")]
+        public required string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Id da Loja")]
+        public required string LojaId { get; set; }
+
+        [ForeignKey("LojaId")]
+        public required Loja Loja { get; set; }
+
+        public ICollection<DetalhesJogo>? DetalhesJogo { get; set; }
+
+    }
+}
---
+++ src/InfoGames/Models/Loja.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InfoGames.Models {
+    [Table("Lojas")]
+    public class Loja {
+        [Key]
+        public required string Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome")]
+        public required string Nome { get; set; }
+
+        public string? Url { get; set; }
+
+        public string? Logo { get; set; }
+
+        public string? ChaveApi { get; set; }
+
+        public ICollection<Jogo>? Jogos { get; set; }
+    }
+}
