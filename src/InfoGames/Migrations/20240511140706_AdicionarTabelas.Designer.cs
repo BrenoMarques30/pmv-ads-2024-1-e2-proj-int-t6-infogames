1f42ed8bcea86a1d88c535515c14d8dfaaa8b912
RafaelMourao <mourao.ea@gmail.com>
2024-05-11 11:08:21 -0300

    fix: corrige noticias quebrando apos merge

--- src/InfoGames/Migrations/20240511140706_AdicionarTabelas.Designer.cs
+++ src/InfoGames/Migrations/20240511140706_AdicionarTabelas.Designer.cs
@@ -4,6 +4,7 @@ using InfoGames.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InfoGames.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240511140706_AdicionarTabelas")]
+    partial class AdicionarTabelas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
